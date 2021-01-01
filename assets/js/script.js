08fe22ffab53f44ccbd392b5f5f5a8547efc243f
Nina Jade <jaderiver64@gmail.com>
2021-01-01 01:04:12 -0600

    HTML and js adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var pageContentEl = document.querySelector("#page-content");
-
+var highScoresEl = document.querySelector("#scores");
+var timerEl = document.querySelector("timer");
 var quizStart = function(event) {
     var targetEl = event.target;
-    pageContentEl.innerHTML = "What?";
+    pageContentEl.innerHTML = "!! Insert Quiz Questions Here !!";
 }
 pageContentEl.addEventListener("click", quizStart);
