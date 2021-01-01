8b490a18400e96a0ee01adfbd58e30221da87942
Nina Jade <jaderiver64@gmail.com>
2021-01-01 01:36:28 -0600

    timer added and working!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
 var pageContentEl = document.querySelector("#page-content");
 var highScoresEl = document.querySelector("#scores");
-var timerEl = document.querySelector("timer");
+var timerEl = document.querySelector("#timer");
+var startBtn = document.querySelector("#start-btn");
+var buttonTry = document.getElementById("start-btn");
+
 var quizStart = function(event) {
-    var targetEl = event.target;
-    pageContentEl.innerHTML = "!! Insert Quiz Questions Here !!";
+    //  var targetEl = event.target;
+    // TO DO : Write questions and function to switch between them
+    pageContentEl.innerHTML = "!! Insert Quiz Question Code Here !!";
 }
-pageContentEl.addEventListener("click", quizStart);
+
+function quizTimer() {
+    var timeRemaining = 60;
+    var timeInterval = setInterval(function() {
+        timerEl.textContent = "Timer: " + timeRemaining;
+        if (timeRemaining > 0) {
+            timeRemaining--;
+        } else {
+            clearInterval(timeInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function endQuiz() {
+    pageContentEl.innerHTML = "!! Insert End Code Here !!";
+}
+startBtn.addEventListener("click", quizStart);
+buttonTry.onclick = quizTimer;
