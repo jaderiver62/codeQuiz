726fa2e929b9551813f8f4b44979d732a327719c
Nina Jade <jaderiver64@gmail.com>
2021-01-04 00:26:34 -0600

    form structure is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var pageContentEl = document.querySelector("#page-content");
 var highScoresEl = document.querySelector("#scores");
 var timerEl = document.querySelector("#timer");
-var startBtn = document.querySelector("#start-btn");
-
-
-var myQuestions = [question1 = {
+var startBtn = document.getElementById("start-btn");
+var pageFormEl = document.querySelector("#page-form");
+var index = 0;
+var score = 0;
+var myQuestions = [{
         currentQuestion: "What is this question?",
         a: "a",
         b: "b",
@@ -12,47 +13,37 @@ var myQuestions = [question1 = {
         d: "d",
         answer: "a"
     },
-    question2 = {
+    {
         currentQuestion: "What is this question?",
-        a: "a",
-        b: "b",
-        c: "c",
-        d: "d",
-        answer: "b"
+        a: "meow1",
+        b: "meow2",
+        c: "meow3",
+        d: "meow4",
+        answer: "meow4"
     },
-    question3 = {
+    {
         currentQuestion: "What is this question?",
-        a: "a",
-        b: "b",
-        c: "c",
-        d: "d",
-        answer: "c"
+        a: "ameow",
+        b: "bmeow",
+        c: "cmeow",
+        d: "dmeow",
+        answer: "cmeow"
     }
 ];
 
 
 
 var quizStart = function(event) {
-    var index = 0;
-    questionSessionHandler(index);
-    //var questionEl = pageContentEl.createElement("li");
-
-}
-var questionSessionHandler = function(index) {
-
+    event.preventDefault();
     var thisQuestion = myQuestions[index];
-    pageContentEl.innerHTML = "<h1>" + thisQuestion.currentQuestion +
-        "</h1><h4><br><button id='question-selection' type='submit'>1.  " +
+    pageContentEl.innerHTML = "<h1>" + thisQuestion.currentQuestion;
+    pageFormEl.innerHTML = "</h1><h4><br><button id='question-selection' type='submit'>1.  " +
         thisQuestion.a + "</button><br><button id='question-selection' type='submit'>2.  " +
         thisQuestion.b + "</button><br><button id='question-selection' type='submit'>3.  " +
-        thisQuestion.c + "</button><br><button id='question-selection' type='submit'> 4.  " +
+        thisQuestion.c + "</button><br><button id='question-selection' type='submit'>4.  " +
         thisQuestion.d + "</button></h4>";
-
+    pageContentEl.appendChild(pageFormEl);
 }
-var checkAnswerHandler = function() {
-
-}
-
 
 function quizTimer() {
     var timeRemaining = 60;
@@ -70,6 +61,27 @@ function quizTimer() {
 function endQuiz() {
     pageContentEl.innerHTML = "!! Insert End Code Here !!";
 }
-startBtn.addEventListener("click", quizStart);
+
+function checkAnswerHandler(event) {
+    /*
+        var answerToCheck = document.getElementById("question-selection");
+        var footEl = document.getElementsByTagName("footer");
+        if (answerToCheck.matches(myQuestions[index].answer)) {
+            footEl.textContent = "Correct!";
+        } else { footEl.textContent = "Wrong!"; }
+        index++;
+        if (timeRemaining > 0 && index <= myQuestions.length) {
+            quizStart(onchange);
+        } else {
+            endQuiz();
+        }*/
+    event.preventDefault;
+    console.log(event);
+    pageContentEl.innerHTML = "What?"
+}
+
 startBtn.onclick = quizTimer;
-questionButton.addEventListener("click", checkAnswerHandler);
+if (pageFormEl) {
+    pageFormEl.addEventListener("click", checkAnswerHandler, true);
+}
+if (startBtn) { startBtn.addEventListener("click", quizStart, true); }
