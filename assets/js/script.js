8655af4a71d45ea7167cd2d76a54fb197d42af76
Nina Jade <jaderiver64@gmail.com>
2021-01-04 16:05:31 -0600

    buttons made into comprehensive list

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,36 @@ var index = 0;
 var score = 0;
 
 var myQuestions = [{
-        currentQuestion: "What is this question?",
-        a: "a",
-        b: "b",
-        c: "c",
-        d: "d",
-        answer: "a"
+        currentQuestion: "What are the parameters of the subtr() method?",
+        a: "substr(start, stop)",
+        b: "substr(start, length)",
+        c: "substr(stop, search)",
+        d: "substr(length,stop)",
+        answer: "substr(start, length)"
     },
     {
-        currentQuestion: "What is this question?",
-        a: "meow1",
-        b: "meow2",
-        c: "meow3",
-        d: "meow4",
-        answer: "meow4"
+        currentQuestion: "Which of the following is a type of error that can occur? ",
+        a: "Syntax errors",
+        b: "Runtime errors",
+        c: "Debugging errors",
+        d: "Logic errors",
+        answer: "Debugging errors"
     },
     {
-        currentQuestion: "What is this question?",
-        a: "ameow",
-        b: "bmeow",
-        c: "cmeow",
-        d: "dmeow",
-        answer: "cmeow"
+        currentQuestion: "Which of the following is the correct syntax for the .getElementsById() method in the supplid HTML? <p id='demo'>Click the button to change the text in this paragraph. </p>",
+        a: "function myFunction() {document.getElementById('#demo').innerHTML ='Hello World'}",
+        b: "function myFunction() {document.getElementById(demo).innerHTML = 'Hello World'}",
+        c: "function myFunction() {document.getElementById([demo]).innerHTML = 'Hello World'}",
+        d: "function myFunction() {document.getElementById('demo').innerHTML ='Hello World'}",
+        answer: "function myFunction() {document.getElementById('demo').innerHTML ='Hello World'}"
+    },
+    {
+        currentQuestion: "What does the method closest() do?",
+        a: "Traverses the element and its parents in the document tree, and the traversing continues until the first node is found that matches the provided selector string.",
+        b: "Used to get the value of an attribute of the particular element.",
+        c: "Determines whether or not the specified substring is present in the given string.",
+        d: "Used to evaluate the closest expression.",
+        answer: "Traverses the element and its parents in the document tree, and the traversing continues until the first node is found that matches the provided selector string."
     }
 ];
 
@@ -42,38 +50,56 @@ var quizStart = function(event) {
         pageContentEl.innerHTML = "";
         event.preventDefault();
         var pageFormEl = document.createElement("div");
+
+        pageFormEl.setAttribute('style', 'display: flex;');
+        pageFormEl.setAttribute('style', 'flex-direction:column;');
         pageFormEl.className = "question-content";
+        var questionText = document.createTextNode(myQuestions[index].currentQuestion);
+        pageFormEl.appendChild(questionText);
+
+        var listEl = document.createElement("UL");
+        listEl.setAttribute("style", "list-style-type:none;");
+        var listItemEl = document.createElement("LI");
 
         var buttonEl = document.createElement("button");
         buttonEl.textContent = "1.  " + myQuestions[index].a;
         buttonEl.setAttribute("index-number", index);
         buttonEl.setAttribute("value", myQuestions[index].a);
-        pageFormEl.appendChild(buttonEl);
+        listItemEl.appendChild(buttonEl);
+        listEl.appendChild(listItemEl);
         buttonEl.addEventListener("click", checkAnswerHandler);
 
+        var listItemEl = document.createElement("LI");
         var buttonEl = document.createElement("button");
         buttonEl.textContent = "2.  " + myQuestions[index].b;
         buttonEl.setAttribute("index-number", index);
         buttonEl.setAttribute("value", myQuestions[index].b);
-        pageFormEl.appendChild(buttonEl);
+        listItemEl.appendChild(buttonEl);
+        listEl.appendChild(listItemEl);
         buttonEl.addEventListener("click", checkAnswerHandler);
 
+        var listItemEl = document.createElement("LI");
         var buttonEl = document.createElement("button");
         buttonEl.textContent = "3.  " + myQuestions[index].c;
         buttonEl.setAttribute("index-number", index);
         buttonEl.setAttribute("value", myQuestions[index].c);
-        pageFormEl.appendChild(buttonEl);
+        listItemEl.appendChild(buttonEl);
+        listEl.appendChild(listItemEl);
         buttonEl.addEventListener("click", checkAnswerHandler);
 
 
+        var listItemEl = document.createElement("LI");
         var buttonEl = document.createElement("button");
         buttonEl.textContent = "4.  " + myQuestions[index].d;
         buttonEl.setAttribute("index-number", index);
         buttonEl.setAttribute("value", myQuestions[index].d);
-        pageFormEl.appendChild(buttonEl);
-
-        pageContentEl.appendChild(pageFormEl);
+        listItemEl.appendChild(buttonEl);
+        listEl.appendChild(listItemEl);
         buttonEl.addEventListener("click", checkAnswerHandler);
+
+        pageFormEl.appendChild(listEl);
+        pageContentEl.appendChild(pageFormEl);
+
     } else {
         endQuiz();
     }
@@ -81,7 +107,7 @@ var quizStart = function(event) {
 }
 
 function quizTimer() {
-    var timeRemaining = 60;
+    var timeRemaining = 20;
     var timeInterval = setInterval(function() {
         timerEl.textContent = "Time: " + timeRemaining;
         if (timeRemaining > 0) {
@@ -92,6 +118,7 @@ function quizTimer() {
         }
     }, 1000);
 }
+
 
 
 function checkAnswerHandler(event) {
@@ -107,11 +134,13 @@ function checkAnswerHandler(event) {
         questionResult = "Wrong!";
     }
     resultDiv.innerHTML = "<h3>" + questionResult + "</h3>";
-    /* correctAnswer + ": " + selectedAnswer; */
 
     index++;
     pageContentEl.addEventListener("click", quizStart);
 }
+
+
+
 
 function endQuiz() {
     pageContentEl.innerHTML = "!! Insert End Code Here !!";
